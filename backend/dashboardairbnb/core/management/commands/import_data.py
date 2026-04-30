39944bc53eae2541c232cbb496e658809eb7322f
LuizzDutra <luizzidutra@gmail.com>
2026-04-30 19:44:31 -0300

    Listings model

--- backend/dashboardairbnb/core/management/commands/import_data.py
+++ backend/dashboardairbnb/core/management/commands/import_data.py
@@ -1,16 +1,16 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
-from core.models import Host
+from core.models import Host, Listing
 import os
 
+class Import():
+    filepath = "core/csv/"
 
-class Command(BaseCommand):
-    help = "Import csv data"
-    
-    def handle(self, *args, **kwargs):
-        filepath = "core/csv/host_df.csv"
-        df = pd.read_csv(filepath)
-        
+    @staticmethod
+    def import_hosts():
+        host_file = "host_df.csv"
+        df = pd.read_csv(Import.filepath + host_file)
+
         instances = [
                 Host(
                     host_id = row['host_id'],
@@ -21,5 +21,45 @@ class Command(BaseCommand):
             ]
         Host.objects.all().delete()
         Host.objects.bulk_create(instances)
-        self.stdout.write(self.style.SUCCESS("Data imported with success"))
-        
+    
+    @staticmethod
+    def import_listings():
+        listing_file = "listings.csv"
+        df = pd.read_csv(Import.filepath + listing_file)
+        instances = [
+                Listing(
+                    id=row['id'],
+                    name=row['name'],
+                    host_id=row['host_id'],
+                    neighbourhood_group=row['neighbourhood_group'],
+                    neighbourhood=row['neighbourhood'],
+                    latitude=row['latitude'],
+                    longitude=row['longitude'],
+                    room_type=row['room_type'],
+                    price=row['price'],
+                    minimum_nights=row['minimum_nights'],
+                    number_of_reviews=row['number_of_reviews'],
+                    last_review=row['last_review'],
+                    reviews_per_month=row['reviews_per_month'],
+                    availability_365=row['availability_365'],
+                    number_of_reviews_ltm=row['number_of_reviews_ltm'],
+                    license=row['license'],
+                    rating=row['rating'],
+                    bedrooms=row['bedrooms'],
+                    beds=row['beds'],
+                    baths=row['baths']
+                    )
+                for _, row in df.iterrows()
+                ]
+        Listing.objects.all().delete()
+        Listing.objects.bulk_create(instances)
+
+class Command(BaseCommand):
+    help = "Import csv data"
+    
+    def handle(self, *args, **kwargs):
+        Import.import_hosts()
+        self.stdout.write(self.style.SUCCESS("Hosts imported with success"))
+
+        Import.import_listings() 
+        self.stdout.write(self.style.SUCCESS("Listings imported with successs"))
--- backend/dashboardairbnb/core/models.py
+++ backend/dashboardairbnb/core/models.py
@@ -1,12 +1,38 @@
 from django.db import models
 
-# Create your models here.
 
 class Listing(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    host_id = models.IntegerField()
+    neighbourhood_group = models.CharField(max_length=100)
+    neighbourhood = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    room_type = models.CharField(max_length=50)
+    price = models.FloatField()
+    minimum_nights = models.IntegerField()
+    number_of_reviews = models.IntegerField()
+    last_review = models.DateField(null=True, blank=True) 
+    reviews_per_month = models.FloatField(null=True, blank=True) 
+    availability_365 = models.IntegerField()
+    number_of_reviews_ltm = models.IntegerField()
+    license = models.CharField(max_length=255, null=True, blank=True) 
+    rating = models.FloatField(null=True, blank=True) 
+    bedrooms = models.FloatField(null=True, blank=True) 
+    beds = models.IntegerField(null=True, blank=True) 
+    baths = models.FloatField(max_length=50, null=True, blank=True) 
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Listing"
+        verbose_name_plural = "Listings"
 
 class Host(models.Model):
     host_id = models.PositiveBigIntegerField(primary_key=True)
     host_name = models.TextField()
     calculated_host_listings_count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.host_name + " -- " + str(self.calculated_host_listings_count)
