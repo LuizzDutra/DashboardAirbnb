a1fdea63f2a7a7b7ca3ed8187a5976d8e517a012
LuizzDutra <luizzidutra@gmail.com>
2026-04-30 20:09:03 -0300

    Basic neighbourhood rating mean

--- backend/dashboardairbnb/core/views.py
+++ backend/dashboardairbnb/core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from . import models
+from django.db.models import Avg
 
 # Create your views here.
 
 def hello(request: HttpResponse):
     return HttpResponse("Hello, world")
+
+def ratings(request: HttpRequest):
+    query = models.Listing.objects.values('neighbourhood_group')
+    results = query.annotate(average_rating = Avg('rating')).order_by('average_rating')
+    return HttpResponse(results)
